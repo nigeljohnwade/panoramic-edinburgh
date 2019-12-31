7a0720f5783fc1d7d9b7342afd8fb77067806fc5
nigeljohnwade <nigeljohnwade@gmail.com>
2019-12-31 00:40:20 +0000

    List Resources View Add card components and styles

--- frontend/src/components/pages/ViewResources.jsx
+++ frontend/src/components/pages/ViewResources.jsx
@@ -13,22 +13,30 @@ const ViewResources = () => {
             .then(resources => setResources(resources));
     }, []);
 
-    return(
-    <div className='panel'>
-        {
-            resources.length > 0 &&
+    return (
+        <div className='panel'>
+            {
+                resources.length > 0 &&
                 resources.map(item => (
                     <div
                         className='resource'
                         key={item.id}
                     >
-                        <p>{item.title}</p>
-                        <p>{item.type}</p>
+                        <div className='card'>
+                            <div className='card-content'>
+                                <div className='card-header'>
+                                    <h3 className='h4'>{item.title}</h3>
+                                </div>
+                                <div className='card-footer'>
+                                    <p>{item.type}</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 ))
-        }
-    </div>
-    )
+            }
+        </div>
+    );
 };
 
 export default ViewResources;
