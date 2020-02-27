d2d1a2a3d760371aa2992b363f7333c60b8e3381
Nigel Wade <nigeljohnwade@gmail.com>
2020-02-27 13:29:06 +0000

    Amplify

--- backend/src/graphql/queries.js
+++ backend/src/graphql/queries.js
@@ -1,32 +1,9 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getResource = `query GetResource($id: ID!) {
-  getResource(id: $id) {
-    id
-    type
-    title
-    shortText
-    descriptiveText
-    primaryImageUrl
-    sourceImageUrl
-    panoramicImageUrl
-    latitude
-    longitude
-    elevation
-    createdAt
-    updatedAt
-    owner
-  }
-}
-`;
-export const listResources = `query ListResources(
-  $filter: ModelResourceFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  listResources(filter: $filter, limit: $limit, nextToken: $nextToken) {
-    items {
+export const getResource = /* GraphQL */ `
+  query GetResource($id: ID!) {
+    getResource(id: $id) {
       id
       type
       title
@@ -38,81 +15,188 @@ export const listResources = `query ListResources(
       latitude
       longitude
       elevation
+      childResources {
+        id
+        type
+        title
+        shortText
+        descriptiveText
+        primaryImageUrl
+        sourceImageUrl
+        panoramicImageUrl
+        latitude
+        longitude
+        elevation
+        childResources {
+          id
+          type
+          title
+          shortText
+          descriptiveText
+          primaryImageUrl
+          sourceImageUrl
+          panoramicImageUrl
+          latitude
+          longitude
+          elevation
+          createdAt
+          updatedAt
+          owner
+        }
+        createdAt
+        updatedAt
+        owner
+      }
       createdAt
       updatedAt
       owner
     }
-    nextToken
   }
-}
 `;
-export const resourceByType = `query ResourceByType(
-  $type: ResourceType
-  $sortDirection: ModelSortDirection
-  $filter: ModelResourceFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  resourceByType(
-    type: $type
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+export const listResources = /* GraphQL */ `
+  query ListResources(
+    $filter: ModelResourceFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      id
-      type
-      title
-      shortText
-      descriptiveText
-      primaryImageUrl
-      sourceImageUrl
-      panoramicImageUrl
-      latitude
-      longitude
-      elevation
-      createdAt
-      updatedAt
-      owner
+    listResources(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        type
+        title
+        shortText
+        descriptiveText
+        primaryImageUrl
+        sourceImageUrl
+        panoramicImageUrl
+        latitude
+        longitude
+        elevation
+        childResources {
+          id
+          type
+          title
+          shortText
+          descriptiveText
+          primaryImageUrl
+          sourceImageUrl
+          panoramicImageUrl
+          latitude
+          longitude
+          elevation
+          createdAt
+          updatedAt
+          owner
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
     }
-    nextToken
   }
-}
 `;
-export const resourceByOwnerByDateUpdated = `query ResourceByOwnerByDateUpdated(
-  $owner: String
-  $updatedAt: ModelStringKeyConditionInput
-  $sortDirection: ModelSortDirection
-  $filter: ModelResourceFilterInput
-  $limit: Int
-  $nextToken: String
-) {
-  resourceByOwnerByDateUpdated(
-    owner: $owner
-    updatedAt: $updatedAt
-    sortDirection: $sortDirection
-    filter: $filter
-    limit: $limit
-    nextToken: $nextToken
+export const resourceByType = /* GraphQL */ `
+  query ResourceByType(
+    $type: ResourceType
+    $sortDirection: ModelSortDirection
+    $filter: ModelResourceFilterInput
+    $limit: Int
+    $nextToken: String
   ) {
-    items {
-      id
-      type
-      title
-      shortText
-      descriptiveText
-      primaryImageUrl
-      sourceImageUrl
-      panoramicImageUrl
-      latitude
-      longitude
-      elevation
-      createdAt
-      updatedAt
-      owner
+    resourceByType(
+      type: $type
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        type
+        title
+        shortText
+        descriptiveText
+        primaryImageUrl
+        sourceImageUrl
+        panoramicImageUrl
+        latitude
+        longitude
+        elevation
+        childResources {
+          id
+          type
+          title
+          shortText
+          descriptiveText
+          primaryImageUrl
+          sourceImageUrl
+          panoramicImageUrl
+          latitude
+          longitude
+          elevation
+          createdAt
+          updatedAt
+          owner
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
     }
-    nextToken
   }
-}
+`;
+export const resourceByOwnerByDateUpdated = /* GraphQL */ `
+  query ResourceByOwnerByDateUpdated(
+    $owner: String
+    $updatedAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelResourceFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    resourceByOwnerByDateUpdated(
+      owner: $owner
+      updatedAt: $updatedAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        type
+        title
+        shortText
+        descriptiveText
+        primaryImageUrl
+        sourceImageUrl
+        panoramicImageUrl
+        latitude
+        longitude
+        elevation
+        childResources {
+          id
+          type
+          title
+          shortText
+          descriptiveText
+          primaryImageUrl
+          sourceImageUrl
+          panoramicImageUrl
+          latitude
+          longitude
+          elevation
+          createdAt
+          updatedAt
+          owner
+        }
+        createdAt
+        updatedAt
+        owner
+      }
+      nextToken
+    }
+  }
 `;
