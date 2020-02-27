561436544d8e2c9d15212157a77b2a012c679f60
nigeljohnwade <nigeljohnwade@gmail.com>
2020-02-27 23:15:44 +0000

    Typo

--- frontend/src/components/pages/EditResource.jsx
+++ frontend/src/components/pages/EditResource.jsx
@@ -1,13 +1,5 @@
-import React, {
-    memo,
-    useEffect,
-    useReducer,
-    useState,
-} from 'react';
-import {
-    Link, Redirect,
-    withRouter,
-} from 'react-router-dom';
+import React, { memo, useEffect, useReducer, useState, } from 'react';
+import { Link, Redirect, withRouter, } from 'react-router-dom';
 
 import apiCalls from '../../api/utilities';
 import Fetching from '../organisms/Fetching';
@@ -17,12 +9,12 @@ import SelectGroup from '../molecules/SelectGroup';
 const formStateReducer = (state, action) => {
     switch (action.type) {
         case 'onChangeStringValue':
-            return { ...state, [action.payload.name]: action.payload.value };
+            return {...state, [action.payload.name]: action.payload.value};
         case 'setState': {
-            return { ...action.payload }
+            return {...action.payload};
         }
         case 'mergeState': {
-            return { ...state, ...action.payload }
+            return {...state, ...action.payload};
         }
         default:
             return state;
@@ -42,7 +34,7 @@ const initialArg = {
 
 const EditResource = ({
     match,
-    location: { pathname },
+    location: {pathname},
 }) => {
     const [resource, setResource] = useState(null);
     const [formState, dispatchFormState] = useReducer(formStateReducer, initialArg);
@@ -87,97 +79,95 @@ const EditResource = ({
                     >
                         Cancel
                     </Link>
-                    <div>
-                        <article className='resource-detail'>
-                            <>
-                                {
-                                    !fetching && !completed &&
-                                    <form
-                                        className='stack create-resource'
-                                        onSubmit={(e) => {
-                                            setFetching(true);
-                                            e.preventDefault();
-                                            apiCalls.updateResource({
-                                                ...formState
-                                            }).then(() => {
-                                                setFetching(false);
-                                                setCompleted(true);
-                                            });
-                                        }}
-                                    >
-                                        <h1>Edit Resource</h1>
-                                        <InputGroup
-                                            field='title'
-                                            label='Title'
-                                            onChange={(e) => changeHandlerString(e)}
-                                            value={formState.title}
-                                        />
-                                        <SelectGroup
-                                            label='Type'
-                                            field='type'
-                                            value={formState.type}
-                                            onChange={(e) => changeHandlerString(e)}
-                                            options={[
-                                                { value: '', label: 'Please select...' },
-                                                { value: 'PANORAMA', label: 'Panorama' },
-                                                { value: 'SITE', label: 'Site' },
-                                                { value: 'JOURNEY', label: 'Journey' },
-                                                { value: 'TOUR', label: 'Tour' },
-                                            ]}
-                                        />
-                                        <InputGroup
-                                            field='shortText'
-                                            label='Short Text'
-                                            onChange={(e) => changeHandlerString(e)}
-                                            value={formState.shortText}
-                                        />
-                                        <div className='form-group input-group'>
+                    <article className='resource-detail'>
+                        <>
+                            {
+                                !fetching && !completed &&
+                                <form
+                                    className='stack create-resource'
+                                    onSubmit={(e) => {
+                                        setFetching(true);
+                                        e.preventDefault();
+                                        apiCalls.updateResource({
+                                            ...formState
+                                        }).then(() => {
+                                            setFetching(false);
+                                            setCompleted(true);
+                                        });
+                                    }}
+                                >
+                                    <h1>Edit Resource</h1>
+                                    <InputGroup
+                                        field='title'
+                                        label='Title'
+                                        onChange={(e) => changeHandlerString(e)}
+                                        value={formState.title}
+                                    />
+                                    <SelectGroup
+                                        label='Type'
+                                        field='type'
+                                        value={formState.type}
+                                        onChange={(e) => changeHandlerString(e)}
+                                        options={[
+                                            {value: '', label: 'Please select...'},
+                                            {value: 'PANORAMA', label: 'Panorama'},
+                                            {value: 'SITE', label: 'Site'},
+                                            {value: 'JOURNEY', label: 'Journey'},
+                                            {value: 'TOUR', label: 'Tour'},
+                                        ]}
+                                    />
+                                    <InputGroup
+                                        field='shortText'
+                                        label='Short Text'
+                                        onChange={(e) => changeHandlerString(e)}
+                                        value={formState.shortText}
+                                    />
+                                    <div className='form-group input-group'>
                                             <textarea
                                                 name='descriptiveText'
                                                 id='descriptiveText'
                                                 value={formState.descriptiveText}
                                                 onChange={(e) => changeHandlerString(e)}
                                             />
-                                            <label htmlFor='descriptiveText'>
-                                                Descriptive Text
-                                            </label>
-                                        </div>
-                                        <InputGroup
-                                            field={'latitude'}
-                                            label={'Latitude'}
-                                            onChange={(e) => changeHandlerString(e)}
-                                            value={formState.latitude}
-                                        />
-                                        <InputGroup
-                                            field={'longitude'}
-                                            label={'Longitude'}
-                                            onChange={(e) => changeHandlerString(e)}
-                                            value={formState.longitude}
-                                        />
-                                        <div className='form group button-group'>
-                                            <button
-                                                className={'save-button'}
-                                                type="submit"
-                                            >
-                                                Save
-                                            </button>
-                                        </div>
-                                    </form>
-                                }
-                            </>
-                            {
-                                fetching &&
-                                <Fetching />
+                                        <label htmlFor='descriptiveText'>
+                                            Descriptive Text
+                                        </label>
+                                    </div>
+                                    <InputGroup
+                                        field={'latitude'}
+                                        label={'Latitude'}
+                                        onChange={(e) => changeHandlerString(e)}
+                                        value={formState.latitude}
+                                    />
+                                    <InputGroup
+                                        field={'longitude'}
+                                        label={'Longitude'}
+                                        onChange={(e) => changeHandlerString(e)}
+                                        value={formState.longitude}
+                                    />
+                                    <div className='form group button-group'>
+                                        <button
+                                            className={'save-button'}
+                                            type="submit"
+                                        >
+                                            Save
+                                        </button>
+                                    </div>
+                                </form>
                             }
-                            {
-                                completed &&
-                                <Redirect to={`/view-resources/view-resource/${id}`} />
-                            }
-                            <footer>
-                                <p> {resource.type}</p>
-                            </footer>
-                        </article>
-                    </div>
+                        </>
+                        {
+                            fetching &&
+                            <Fetching/>
+                        }
+                        {
+                            completed &&
+                            <Redirect to={`/view-resources/view-resource/${id}`}/>
+                        }
+                        <footer>
+                            <p> {resource.type}</p>
+                        </footer>
+                    </article>
                 </>
             }
         </div>
