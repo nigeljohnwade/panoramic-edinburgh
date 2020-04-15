996e43f9b5848f64e819d77de04b8c967197fc69
nigeljohnwade <nigeljohnwade@gmail.com>
2020-04-15 17:23:52 +0100

    Update front end config for backend

--- frontend/src/config/aws-exports-dev.js
+++ frontend/src/config/aws-exports-dev.js
@@ -2,14 +2,14 @@
 
 const awsmobile = {
     "aws_project_region": "eu-west-1",
-    "aws_appsync_graphqlEndpoint": "https://25wdg73ruzav7e4ztalqdybuzq.appsync-api.eu-west-1.amazonaws.com/graphql",
-    "aws_appsync_region": "eu-west-1",
-    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_cognito_identity_pool_id": "eu-west-1:69eefbf5-c465-4948-845c-d81b6fb09965",
+    "aws_cognito_identity_pool_id": "eu-west-1:00071866-27a5-40a5-8f37-63a1a501357a",
     "aws_cognito_region": "eu-west-1",
-    "aws_user_pools_id": "eu-west-1_dCmQlqTgx",
-    "aws_user_pools_web_client_id": "7g4u8hqm8i40octni05p6jrsr5",
-    "oauth": {}
+    "aws_user_pools_id": "eu-west-1_Mmcp7oYFt",
+    "aws_user_pools_web_client_id": "3skrm32utkm02vfggqb7m1brr8",
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://h22frqy2kfbznpk2wwj4bd53ve.appsync-api.eu-west-1.amazonaws.com/graphql",
+    "aws_appsync_region": "eu-west-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS"
 };
 
 
